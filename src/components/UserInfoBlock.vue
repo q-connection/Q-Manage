56fa8e56c9ca1ba494635d0cfefb255a180248c2
luan <pmluan1998@gmail.com>
2022-07-25 15:12:51 +0700

    fix

--- src/components/UserInfoBlock.vue
+++ src/components/UserInfoBlock.vue
@@ -13,17 +13,19 @@
             <hr>
             <div v-if="not_working_users.length > 0">
                 <div class="d-flex">
-                    <h5>Not working today</h5>
-                    <img src="/images/icons/down.png" :class="[{'not-show': !is_show_working_users},'ml-auto', 'icon-down']"  @click="is_show_working_users=!is_show_working_users" />
+                    <h5 class="title-notworking">Not working today</h5>
+                    <img src="/images/icons/down.png"
+                        :class="[{ 'not-show': !is_show_working_users }, 'ml-auto', 'icon-down']"
+                        @click="is_show_working_users = !is_show_working_users" />
                 </div>
                 <div class="user-notworking-wrapper" v-show="is_show_working_users">
                     <div class="notworking-item" v-for="(u, index) in not_working_users" :key="index">
-                        {{ u }}
+                        <div class="user-name-notworking">{{ u }}</div>
                     </div>
                 </div>
             </div>
             <div class="text-right mt-2">
-                <h5 class="text-primary text-cursor" @click="$bvModal.show('modal-organization')">
+                <h5 class="text-primary text-cursor organizational-chart" @click="$bvModal.show('modal-organization')">
                     Organizational Charts
                 </h5>
             </div>
@@ -95,10 +97,38 @@ export default {
 .user-info {
     padding-left: .5rem;
 
+    .title-notworking {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 22px;
+    }
+
+
     .user-notworking-wrapper {
-        margin-top: 10px;
-        max-height: 150px;
+        margin-top: 5px;
+        max-height: 160px;
         overflow: auto;
+
+        &::-webkit-scrollbar {
+            width: 5px;
+        }
+
+        &::-webkit-scrollbar:horizontal {
+            height: auto;
+            max-height: 99px;
+            background-color: red;
+        }
+
+        &::-webkit-scrollbar-track {
+            background-color: rgba(255, 255, 255, 0.1);
+            border-radius: 10px;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color: #D9D9D9;
+            border-radius: 10px;
+        }
 
         .notworking-item {
             padding: .25rem .5rem;
@@ -106,7 +136,20 @@ export default {
             background: #F5F5F5;
             border-bottom: 1px solid #d9d9d9;
             margin-bottom: 5px;
+
+            .user-name-notworking {
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 19px;
+            }
         }
     }
+}
+
+.organizational-chart {
+    font-style: italic;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
 }
 </style>
