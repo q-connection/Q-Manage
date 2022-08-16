34a1c362a60d864bd6252f48e7bc98a6da16aff3
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-08-16 14:55:54 +0700

    Fix date

--- src/components/leave-days/Form.vue
+++ src/components/leave-days/Form.vue
@@ -15,6 +15,7 @@
                                             v-model="formData.start_date"
                                             :state="$isValid(errors, valid)"
                                             :error="errors[0]"
+                                            :max="formData.end_date"
                                             required
                                         />
                                     </validation-provider>
@@ -28,14 +29,14 @@
                                             :style="`padding-top: ${$device.mobile ? 0 : '2.25rem'}`"                 
                                         >
                                             <b-form-radio-group
-                                                v-model="formData.time_1"
                                                 :aria-describedby="ariaDescribedby"
+                                                :options="startTimeOptions"
+                                                v-model="startTime"
+                                                :disabled="!formData.start_date"
+                                                @change="checkTimes"
                                                 size="lg"
                                                 name="time_1"
-                                            >
-                                                <b-form-radio value="08:30" :disabled="!formData.start_date">08:30</b-form-radio>
-                                                <b-form-radio value="13:00" :disabled="!formData.start_date">13:00</b-form-radio>
-                                            </b-form-radio-group>
+                                            />
                                         </b-form-group>
                                     </validation-provider>
                                 </b-col>
@@ -52,6 +53,7 @@
                                             v-model="formData.end_date"
                                             :state="$isValid(errors, valid)"
                                             :error="errors[0]"
+                                            :min="formData.start_date"
                                             required
                                         />
                                     </validation-provider>
@@ -65,15 +67,14 @@
                                             :style="`padding-top: ${$device.mobile ? 0 : '2.25rem'}`"               
                                         >
                                             <b-form-radio-group
-                                                v-model="formData.time_2"
                                                 :aria-describedby="ariaDescribedby"
+                                                :options="endTimeOptions"
+                                                v-model="endTime"
+                                                :disabled="!formData.end_date"
+                                                @change="checkTimes"
                                                 size="lg"
                                                 name="time_2"
-                                                :disabled="endTimeDisabled"
-                                            >
-                                                <b-form-radio value="12:00" :disabled="!formData.end_date">12:00</b-form-radio>
-                                                <b-form-radio value="17:30" :disabled="!formData.end_date">17:30</b-form-radio>
-                                            </b-form-radio-group>
+                                            />
                                         </b-form-group>
                                     </validation-provider>
                                 </b-col>
@@ -152,29 +153,31 @@
             }
         },
 
-        data: () => ({
-            isSubmitting: false,
-            startTimeOptions: [{text: '08:30', value: '08:30'},{text: '13:00', value: '13:00'}],
-            endTimeOptions: [{text: '12:00', value: '12:00'},{text: '17:30', value: '17:30'}],
-            typeOptions: [
-                {label: 'Paid Leave - Nghỉ có lương', value: 'paid_leave'},
-                {label: 'Unpaid Leave - Nghỉ không lương', value: 'unpaid_leave'},
-                {label: 'Paternity Leave - Nghỉ thai sản', value: 'paternity_leave'},
-                {label: 'Marriage Leave - Nghỉ đám cưới', value: 'marriage_leave'},
-                {label: 'Sick Leave - Nghỉ bệnh', value: 'sick_leave'}
-            ],
-            approvers: [],
-            formData: {
-                start_date: null,
-                end_date: null,
-                time_1: '13:00',
-                time_2: '17:30',
-                reason: '',
-                type: null,
-                approver_id: null,
-                status: 'pending'
+        data() {
+            return {
+                isSubmitting: false,
+                startTimeOptions: [{text: '08:30', value: '08:30'},{text: '13:00', value: '13:00'}],
+                endTimeOptions: [{text: '12:00', value: '12:00'},{text: '17:30', value: '17:30'}],
+                typeOptions: [
+                    {label: 'Paid Leave - Nghỉ có lương', value: 'paid_leave'},
+                    {label: 'Unpaid Leave - Nghỉ không lương', value: 'unpaid_leave'},
+                    {label: 'Paternity Leave - Nghỉ thai sản', value: 'paternity_leave'},
+                    {label: 'Marriage Leave - Nghỉ đám cưới', value: 'marriage_leave'},
+                    {label: 'Sick Leave - Nghỉ bệnh', value: 'sick_leave'}
+                ],
+                approvers: [],
+                formData: {
+                    start_date: null,
+                    end_date: null,
+                    time_1: '13:00',
+                    time_2: '17:30',
+                    reason: '',
+                    type: null,
+                    approver_id: null,
+                    status: 'pending'
+                }
             }
-        }),
+        },
 
         watch: {
             data(newval) {
@@ -182,6 +185,28 @@
                     this.initFormData()
                 }
             }
+        },
+
+        computed: {
+            startTime: {
+                get() {
+                    return this.formData.time_1
+                },
+
+                set(val) {
+                    this.$set(this.formData, 'time_1', val)
+                }
+            },
+
+            endTime: {
+                get() {
+                    return this.formData.time_2
+                },
+
+                set(val) {
+                    this.$set(this.formData, 'time_2', val)
+                }
+            },
         },
 
         async mounted() {
@@ -236,6 +261,14 @@
                     }
                 } catch (err) {
                     console.log(err)
+                }
+            },
+
+            checkTimes() {
+                if(this.formData.start_date == this.formData.end_date) {
+                    if(this.startTime == '13:00' && this.endTime == '12:00') {
+                        this.endTime = '17:30'
+                    }
                 }
             }
         }
