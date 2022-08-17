eda62b5ba718ef64d1b6b66841aaa33d3f1e5a91
Kiều Trọng Thiện <kieutrongthien@gmail.com>
2022-08-17 10:27:11 +0700

    Add leave day approved page

--- src/router/leave-days.js
+++ src/router/leave-days.js
@@ -4,5 +4,11 @@ export default [
         name: 'leave-days',
         meta: { layout: 'default', requiresAuth: true },
         component: () => import('@/views/LeaveDays.vue')     
-    }
+    },
+    {
+        path: '/leave-day-status',
+        name: 'leave-days-status',
+        meta: { layout: 'blank' },
+        component: () => import('@/views/LeaveDaysStatus.vue')     
+    },
 ]
